5cde823fd481c8b686ccfb185a87e45f59127158
Sergii Golovko <golovko.sergey@gmail.com>
2023-10-02 20:22:56 +0100

    tests refactoring

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -4,8 +4,6 @@ const seed = require("../db/seeds/seed");
 const db = require('../db/connection');
 const data = require('../db/data/test-data')
 
-const endpointsJson = require("../endpoints.json");
-
 beforeEach(()=>{
   return seed(data);
 })
@@ -14,7 +12,33 @@ afterAll(()=>{
   db.end();
 })
 
-// GET /api
+// GET /api/topics
+describe('GET /api/topics', () => { 
+  test('should return array of topics', () => { 
+      return query(app)
+      .get('/api/topics/')
+      .expect(200)
+      .then((res) => {
+          const {topics} = res.body;
+          expect(topics.length).toBe(3);
+          topics.forEach(topic => {
+              expect(typeof topic.description).toBe('string');
+              expect(typeof topic.slug).toBe('string');
+          });
+      })
+   })
+})
+
+ // general
+ describe('general', () => { 
+  test('return 400 if requested endpoint not exist', () => { 
+      return query(app)
+      .get('/api/not-exist/')
+      .expect(400)
+  })
+})
+
+// endpoints - GET /api
 describe("GET /api", () => {
   test("should return an object describing all the available endpoints on API", () => {
     return query(app)
@@ -23,7 +47,6 @@ describe("GET /api", () => {
       .then((res) => {
         const { endpoints } = res.body;
         for(endpoint in endpoints){
-          console.log(endpoints[endpoint]);
           expect(typeof endpoints[endpoint].description).toBe('string');
         }
       });
