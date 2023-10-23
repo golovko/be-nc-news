ddaaac43282c27e7b7be5dbfb2740ef307f0afe0
Sergii Golovko <golovko.sergey@gmail.com>
2023-10-23 09:30:55 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const apiRouter = require('./routes/api-router');
 const { customErrors, psqlErrors } = require('./errors/errorHandler.js');
+const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // using express Router()
 app.use('/api', apiRouter);
 
-// overriding default express error for endpoints that not exist 
+// overriding default express error for endpoints that not exist
 app.use((req, res, next) => {
-    res.status(400).send("Bad request");
-  })
+  res.status(400).send('Bad request');
+});
 
 // error handling
 app.use(psqlErrors);
--- routes/article-router.js
+++ routes/article-router.js
@@ -1,22 +1,27 @@
 const articleRouter = require('express').Router();
-const { getArticleById, getArticles, patchArticle, postArticle, deleteArticle } = require('../controllers/articles.controller.js');
-const { getComments, postComments } = require('../controllers/comments.controller.js');
+const {
+  getArticleById,
+  getArticles,
+  patchArticle,
+  postArticle,
+  deleteArticle,
+} = require('../controllers/articles.controller.js');
+const {
+  getComments,
+  postComments,
+} = require('../controllers/comments.controller.js');
 
+articleRouter.route('/').get(getArticles).post(postArticle);
 
 articleRouter
-.route('/')
-.get(getArticles)
-.post(postArticle)
+  .route('/:article_id')
+  .get(getArticleById)
+  .patch(patchArticle)
+  .delete(deleteArticle);
 
 articleRouter
-.route('/:article_id')
-.get(getArticleById)
-.patch(patchArticle)
-.delete(deleteArticle)
-
-articleRouter
-.route('/:article_id/comments')
-.get(getComments)
-.post( postComments)
+  .route('/:article_id/comments')
+  .get(getComments)
+  .post(postComments);
 
 module.exports = articleRouter;
